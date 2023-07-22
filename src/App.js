b78313eacef421671de9790c5fe754855342b56d
zana-abdollahpour <zana.ap4@gmail.com>
2023-07-22 16:00:05 +0330

    refactor and search city on typing

--- src/App.js
+++ src/App.js
@@ -32,20 +32,17 @@ function formatDay(dateStr) {
   }).format(new Date(dateStr));
 }
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      location: "Tokyo",
-      isLoading: false,
-      displayLocation: "",
-      weather: {},
-    };
+class App extends Component {
+  state = {
+    location: "",
+    isLoading: false,
+    displayLocation: "",
+    weather: {},
+  };
 
-    this.fetchWeather = this.fetchWeather.bind(this);
-  }
+  fetchWeather = async () => {
+    if (this.state.location.length < 2) return this.setState({ weather: {} });
 
-  async fetchWeather() {
     try {
       this.setState({ isLoading: true });
 
@@ -54,12 +51,12 @@ export default class App extends Component {
         `https://geocoding-api.open-meteo.com/v1/search?name=${this.state.location}`
       );
       const geoData = await geoRes.json();
-      // console.log(geoData);
 
       if (!geoData.results) throw new Error("Location not found");
 
       const { latitude, longitude, timezone, name, country_code } =
         geoData.results.at(0);
+
       this.setState({
         displayLocation: `${name} ${convertToFlag(country_code)}`,
       });
@@ -75,21 +72,30 @@ export default class App extends Component {
     } finally {
       this.setState({ isLoading: false });
     }
+  };
+
+  setLocation = (e) => this.setState({ location: e.target.value });
+
+  componentDidMount() {
+    this.setState({ location: localStorage.getItem("location") || "" });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.location !== prevState.location) {
+      this.fetchWeather();
+
+      localStorage.setItem("location", this.state.location);
+    }
   }
 
   render() {
     return (
       <div className="app">
         <h1>Classy Weather</h1>
-        <div>
-          <input
-            type="text"
-            placeholder="Search for location..."
-            value={this.state.location}
-            onChange={(e) => this.setState({ location: e.target.value })}
-          />
-        </div>
-        <button onClick={this.fetchWeather}>Get Weather</button>
+        <Input
+          location={this.state.location}
+          onChangeLocation={this.setLocation}
+        />
 
         {this.state.isLoading && <p className="loader">Loading...</p>}
 
@@ -99,6 +105,23 @@ export default class App extends Component {
             location={this.state.displayLocation}
           />
         )}
+      </div>
+    );
+  }
+}
+
+export default App;
+
+class Input extends Component {
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          placeholder="Search from location..."
+          value={this.props.location}
+          onChange={this.props.onChangeLocation}
+        />
       </div>
     );
   }
@@ -119,11 +142,11 @@ class Weather extends Component {
         <ul className="weather">
           {dates.map((date, i) => (
             <Day
-              key={date}
               date={date}
               max={max.at(i)}
               min={min.at(i)}
               code={codes.at(i)}
+              key={date}
               isToday={i === 0}
             />
           ))}
@@ -142,7 +165,7 @@ class Day extends Component {
         <span>{getWeatherIcon(code)}</span>
         <p>{isToday ? "Today" : formatDay(date)}</p>
         <p>
-          {Math.round(min)}&deg; &mdash; <strong>{Math.round(max)}&deg;</strong>
+          {Math.floor(min)}&deg; &mdash; <strong>{Math.ceil(max)}&deg;</strong>
         </p>
       </li>
     );
